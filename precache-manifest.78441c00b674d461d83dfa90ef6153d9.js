dcd1c25edaf32f65ac5ac1926fe43b1937c0d168
Aibek <aibek.uisinbayev@gmail.com>
2021-07-05 01:42:26 +0600

    Deploy

---
+++ precache-manifest.78441c00b674d461d83dfa90ef6153d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ec6a9a9d5496965f41",
+    "url": "/biznes-samgau/css/app.c9919195.css"
+  },
+  {
+    "revision": "21ad3a6432134cbe0c3825904d31cc7f",
+    "url": "/biznes-samgau/index.html"
+  },
+  {
+    "revision": "a0ec6a9a9d5496965f41",
+    "url": "/biznes-samgau/js/app.cdda16c9.js"
+  },
+  {
+    "revision": "dc15b3e8df7906f68435",
+    "url": "/biznes-samgau/js/chunk-vendors.63e857cc.js"
+  },
+  {
+    "revision": "a112dff36244417e757480114543b292",
+    "url": "/biznes-samgau/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/biznes-samgau/robots.txt"
+  }
+]);
